63d2905e4c7a4a28b4dd679852d23c1444ff2617
Saydeezr <saydeezr@gmail.com>
2024-07-23 15:27:01 -0400

    styled contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,26 @@
 function Contact() {
-    return(
+    return (
         <div>
-            <h2 className='text-2xl font-bold mb-4'>Contact Me</h2>
-            <form>
-                <label htmlFor="fname">Name:</label>
-                <input type="text" id="fname" name="fname" />
-                <label htmlFor="lname">Email:</label>
-                <input type="text" id="lname" name="lname" />
-                <label htmlFor="lname">Message:</label>
-                <input type="text" id="lname" name="lname" />
+            <h2 className='text-2xl font-bold mb-4 py-8'>Contact Me</h2>
+            <form className="max-w-md mx-auto">
+                <div className="relative mb-5">
+                    <input type="email" name="floating_email" id="floating_email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Email Address" required />
+                    </div>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="text" name="floating_first_name" id="floating_first_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="First Name"required />
+                        </div>
+                    <div className="relative z-0 w-full mb-5 group">
+                        <input type="text" name="floating_last_name" id="floating_last_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Last Name" required />
+                        </div>
+                </div>
+                <div className="relative mb-5 group">
+                    <input name="floating_message" id="floating_message" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Message" required></input>
+                    </div>
+                <button type="submit" className="text-white bg-indigo-400 hover:bg-indigo-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-indigo-400 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
             </form>
         </div>
-    )
+    );
 }
 
 export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-export default function Footer() {
+function Footer() {
     return(
         <div className=" rounded-lg shadow m-4 py-8">
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="w-full max-w-screen-xl mx-auto p-4">
                 <div className="sm:flex sm:items-center sm:justify-between">
-                    <div className="shadow m-4 py-8">
+                    <div className="shadow m-4 py-4">
                         <a href="https://github.com/Saydeezr/" target="_blank" rel="noopener noreferrer">
                             <i className="fab fa-github fa-2x text-white-800 hover:text-gray-600 px-2.5"></i>
                         </a> 
@@ -11,33 +11,20 @@ export default function Footer() {
                             <i className="fab fa-linkedin fa-2x text-white-700 hover:text-blue-500 px-2.5"></i>
                         </a>
                     </div>
-            
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-    </div>
-            {/* <div className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-                    <a href="https://github.com/Saydeezr/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-github fa-2x text-gray-800 hover:text-gray-600"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/in/saydeez-reyes-bb9380314/" target="_blank" rel="noopener noreferrer">
-                        <i className="fab fa-linkedin fa-2x text-blue-700 hover:text-blue-500"></i>
-                    </a>
-                </div> */}
+                    <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Home</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline me-4 md:me-6">Top</a>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 Portfolio Rights to Author</span>
+            </div>
         </div>
     )
 }
+
+export default Footer;
