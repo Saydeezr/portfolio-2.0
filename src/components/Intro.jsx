60965b3f66b9d8ea8beac765e9df9403ad416611
Saydeezr <saydeezr@gmail.com>
2024-07-25 16:27:44 -0400

    added logo

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -10,10 +10,7 @@ function Intro() {
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
-                    </a>
-                    <a href="./src/components/About" className="py-3 px-5 sm:ms-4 text-sm font-medium text-gray-900 focus:outline-none bg-gray-800 rounded-lg border border-gray-200 hover:bg-gray-500 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-70">
-                        Learn more
-                    </a>  
+                    </a> 
                 </div>
             </div>
             <img className="absolute top-38 left-96 w-100 h-auto z-0" src="/images/introBackground.png" />
