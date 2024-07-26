0cb2f6bc4c761ea54d6133031581a4a5bd297024
Saydeezr <saydeezr@gmail.com>
2024-07-25 20:45:06 -0400

    fixed positioning and size of hompage intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,19 +3,21 @@ function Intro() {
       <section className="contentContainer flex justify-between items-center h-screen relative ">
         <div className="flex items-center mx-auto max-w-screen-xl lg:py-16 ">
           <div className="text-left z-30">
-            <h1 className="mb-2 font-bold text-gray-900 text-6xl md:text-7xl lg:text-8xl relative z-20">
+            <div className="absolute left-24 top-60">
+              <h1 className="mb-2 font-bold text-gray-900 text-6xl md:text-7xl lg:text-8xl relative z-20">
               Saydeez Reyes
-            </h1>
-            <p className="mb-8 pt-6 text-lg font-normal text-gray-500 lg:text-xl">
+              </h1>
+              <p className="mb-8 pt-6 text-lg font-normal text-gray-500 lg:text-xl">
               | Full-Stack Web Developer
-            </p>
-            <div className="flex-initial space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-              <a href="#" className="inline-flex items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-gray-900 hover:bg-gray-700">
+              </p>
+              <div className="flex-initial space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                <a href="#" className="inline-flex items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-gray-900 hover:bg-gray-700">
                 Get started
-              </a>
+                </a>
+              </div>
             </div>
           </div>
-          <img className="absolute inset-y-15 right-2 w-[650px] h-auto z-10" src="/images/introBackground.png" alt="Intro Background"/>
+          <img className="absolute inset-y-60 right-32 w-[450px] h-auto z-10" src="/images/introBackground.png" alt="Intro Background"/>
         </div>
       </section>
     );
